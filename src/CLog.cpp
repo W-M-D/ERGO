df25123d4f449e019dc1ea15394eb4d2dcaccf6a
W-M-D <matthew.d.weger@gmail.com>
2014-09-09 23:04:42 -0400

    init_commit

--- src/CLog.cpp
+++ src/CLog.cpp
@@ -75,7 +75,7 @@ void CLog::archive_load(std::forward_list <std::string> &  data_list)
       data_in.open( "/etc/ERGO/ERGO_DATA.csv");
       if(!data_in.eof())
       {
-                  data_in.seekg(last_sent_line_get(),std::ios::end);
+                  data_in.seekg(last_sent_line_get());
       }
       else
       {
