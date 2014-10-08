535ee3139304aba8a508e79254ba6fe7e4f2db80
W-M-D <matthew.d.weger@gmail.com>
2014-10-08 00:23:19 -0400

    adding more verbose internet checks

--- src/CERGO_INTERNET.cpp
+++ src/CERGO_INTERNET.cpp
@@ -19,8 +19,8 @@ int CERGO_INTERNET::internet_availiable()
 
     if(curl)
     {
-        curl_easy_setopt(curl,CURLOPT_CONNECTTIMEOUT_MS,400);
-        curl_easy_setopt(curl,CURLOPT_TIMEOUT,3);
+        curl_easy_setopt(curl,CURLOPT_CONNECTTIMEOUT_MS,300);
+        curl_easy_setopt(curl,CURLOPT_TIMEOUT,5);
         curl_easy_setopt(curl, CURLOPT_URL, "data.ergotelescope.org");
         while ((res = curl_easy_perform(curl)) != CURLE_OK)
         {
