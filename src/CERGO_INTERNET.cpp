35b218dc7a6c2d1ecb7b8904ac66ad16f014d8cc
W-M-D <matthew.d.weger@gmail.com>
2014-10-08 00:59:05 -0400

    testing new values

--- src/CERGO_INTERNET.cpp
+++ src/CERGO_INTERNET.cpp
@@ -19,7 +19,8 @@ int CERGO_INTERNET::internet_availiable()
 
     if(curl)
     {
-        curl_easy_setopt(curl,CURLOPT_CONNECTTIMEOUT_MS,800);
+        curl_easy_setopt(curl,CURLOPT_CONNECTTIMEOUT_MS,1000);
+          curl_easy_setopt(curl,CURLOPT_TIMEOUT,6);
         curl_easy_setopt(curl, CURLOPT_URL, "data.ergotelescope.org");
         while ((res = curl_easy_perform(curl)) != CURLE_OK)
         {
