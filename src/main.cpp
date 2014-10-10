5f0c53319359298bd4e704755685b3f225e36131
W-M-D <matthew.d.weger@gmail.com>
2014-10-10 00:24:44 -0400

    trying some threading

--- src/main.cpp
+++ src/main.cpp
@@ -78,7 +78,7 @@ int main(int argc, char *argv[])
 
     while(true) // main management loop
     {
-        std::thread internet_thread((std::bind( &CERGO_INTERNET::manage_list, Internet)));
+        std::thread internet_thread([&] { Internet->manage_list(); });
         internet_thread.detach();
         counter = Serial.data_read(data_list); // checks for incomming data
         while(!data_list.empty())
